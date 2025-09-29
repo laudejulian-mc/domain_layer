eacd23849c157e0676f7b5b25190aa3b9e384a7f
Rey Joseph S. Daprosa <reyjoseph234@gmail.com>
2025-09-29 23:24:15 +0800

    Completed Step 1-8 of the project

---
+++ lib/domain/entities/equatable.dart
@@ -0,0 +1,5 @@
+// This is a placeholder for the Equatable class to avoid errors if the package is not installed.
+abstract class Equatable {
+  const Equatable();
+  List<Object?> get props;
+}
---
+++ lib/domain/usecases/announcement/get_announcement_detail_usecase.dart
@@ -0,0 +1,13 @@
+import '../../entities/announcement_entity.dart';
+import '../../repositories/announcement_repository.dart';
+import '../../entities/either.dart';
+import '../../entities/Failure.dart';
+
+class GetAnnouncementDetailUseCase {
+  final AnnouncementRepository repository;
+  GetAnnouncementDetailUseCase(this.repository);
+
+  Future<Either<Failure, AnnouncementEntity>> call(String id) {
+    return repository.getAnnouncementDetail(id);
+  }
+}
