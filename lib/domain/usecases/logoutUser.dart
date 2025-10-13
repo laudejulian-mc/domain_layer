9c879a8032b0f28341e6955befe45349ee3f3c5a
Rey Bien <caneja.alum@gmail.com>
2025-10-13 12:15:55 +0800

    Your commit message

--- lib/domain/usecases/logoutUser.dart
+++ lib/domain/usecases/logoutUser.dart
@@ -1,9 +1,19 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_application_1/core/errors/failure.dart';
 import 'package:flutter_application_1/domain/repositories/repositoryInterface.dart';
+import 'package:flutter_application_1/domain/repositories/userregister.dart';
 
 class LogoutUser {
   final UserRepository repository;
   LogoutUser(this.repository);
 
+  Future<Either<Failure, void>> call() {
+    return repository.logout();
+  }
+}
+
+/// Minimal repository interface for logout used by LogoutUser.
+/// Move this to a shared repository file if you have a central interface.
+abstract class UserRepository {
+  Future<Either<Failure, void>> logout();
 }
