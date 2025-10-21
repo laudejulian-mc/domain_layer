b447b303f3fbb00c40ac4955a9c29cbe06fcfc78
Mary Cons A. Montejo <montejomarycons@y-3.local>
2025-10-21 23:56:32 +0800

    Refactor: cleaned folder structure and moved domain_layer to features/events

--- lib/flutter_application_1-1/domain_layer/data/schedule/datasources/schedule_data_source.dart
+++
@@ -1,9 +0,0 @@
-// This file defines the ScheduleDataSource interface, which declares methods for interacting with schedule data in Firebase.
-
-abstract class ScheduleDataSource {
-  Future<List<ScheduleModel>> getSchedules();
-  Future<ScheduleModel> getScheduleById(String id);
-  Future<void> createSchedule(ScheduleModel schedule);
-  Future<void> updateSchedule(ScheduleModel schedule);
-  Future<void> deleteSchedule(String id);
-}
